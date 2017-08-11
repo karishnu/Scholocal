f74080da92cbad92606dbfab242acbc7a9384216
TechDetective <vishwajeetsinhjadeja4@gmail.com>
2017-08-11 13:30:30 +0530

    Search Implementation

--- routes/web/search.js
+++ routes/web/search.js
@@ -5,17 +5,15 @@ const searchDb = require('../../database/search');
 
 const authenticate = require('../../authenticate');
 
-router.use(function (req, res, next) {
-    authenticate.verify_cookie(req, res, next);
-});
+// router.use(function (req, res, next) {
+//     authenticate.verify_cookie(req, res, next);
+// });
 
 router.get('/', function (req, res, next) {
     console.log(req.query.text);
    searchDb.search(req.query.text, function (err, doc) {
        console.log(err);
-       res.json({
-           data: doc
-       });
+       res.render('searchResults', {query: req.query.text, data: doc});
    })
 });
 
